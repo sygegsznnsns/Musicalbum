f86a13a07841c304d65a888ca1e9de5b14c6814d
chenziang123 <czza0307@gmail.com>
2025-12-19 14:34:50 +0800

    点滴

--- src/wp-content/plugins/musicalbum-integrations/viewing-records.php
+++ src/wp-content/plugins/musicalbum-integrations/viewing-records.php
@@ -1179,10 +1179,10 @@ final class Viewing_Records {
         $manager_style = '';
         $statistics_style = '';
         if ($manager_bg) {
-            $manager_style = ' style="background-image: url(' . esc_attr($manager_bg) . ');"';
+            $manager_style = ' style="background-image: url(\'' . esc_attr($manager_bg) . '\');"';
         }
         if ($statistics_bg) {
-            $statistics_style = ' style="background-image: url(' . esc_attr($statistics_bg) . ');"';
+            $statistics_style = ' style="background-image: url(\'' . esc_attr($statistics_bg) . '\');"';
         }
         
         ob_start();
